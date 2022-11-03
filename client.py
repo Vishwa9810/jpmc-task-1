8851411abfdf33a9c784d72669e26ed6c2de689b
Vishwa9810 <117373500+Vishwa9810@users.noreply.github.com>
2022-11-03 20:21:54 +0530

    Update client.py

--- client.py
+++ client.py
@@ -18,42 +18,96 @@
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
 
+import urllib.request
+import time
 import json
 import random
-import urllib.request
-
+ 
 # Server API URLs
 QUERY = "http://localhost:8080/query?id={}"
-
+ 
 # 500 server request
 N = 500
-
-
+ 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
+    """ Produce all of the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price+ask_price)/2
     return stock, bid_price, ask_price, price
-
-
+ 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
-
-
+    if (price_b == 0 ):
+        return
+    """ Also create some unit tests for this function in client_test.py """
+    return price_a/price_b
+ 
 # Main
 if __name__ == "__main__":
+ 
     # Query the price once every N seconds.
-    for _ in iter(range(N)):
+    for _ in range(N):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
+ 
         """ ----------- Update to get the ratio --------------- """
+        prices={}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock]=price
+            print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+ 
+        print ("Ratio %s" % (getRatio(prices['ABC'],prices['DEF'])))
+        
+################################################################################        
+        
+ Bonus Task: 
+ 
+step 1: open client_test.py 
+step 2: Select all and backspace and paste the full below code 
 
-        print("Ratio %s" % getRatio(price, price))
+Code Changes in unit Test:
+
+import unittest
+from client3 import getDataPoint
+ 
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self =.assertEqual(getDataPoint(quote), (quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+ 
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self =.assertEqual(getDataPoint(quote), (quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+ 
+ 
+  """ ------------ Add more unit tests ------------ """
+ 
+ 
+ 
+if __name__ == '__main__':
+    unittest.main()
+    
+-------------------------------------------------------------------------------------------------------------    
+Create A Patch file :
+Step 1: Open cmd and change dir by cd <path where you downloaded the git files:
+step 2: write the below code
+
+git add -A
+git config user.email "<your_email_address>"
+git config user.name "<your_name>"
+git commit -m "Create Patch File"
+git format-patch -1 HEAD
